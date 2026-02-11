6082603b65e0d5dbc0301cf9192c5d07b70e415f
saleh <saleherragragui02@gmail.com>
2026-02-11 18:56:33 +0100

    adding templates/home.html

--- apps/products/urls.py
+++ apps/products/urls.py
@@ -4,6 +4,5 @@ from . import views
 app_name = 'products'
 
 urlpatterns = [
-    # We will leave this empty for a moment until we write our first view
-    # path('', views.ProductListView.as_view(), name='list'),
+    path('', views.home_temp, name='home'),
 ]
