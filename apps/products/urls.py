1d8685c7d33d3a852ff9bfd2337233afdd5d54f7
saleh <saleherragragui02@gmail.com>
2026-02-11 18:54:08 +0100

    adding urls.py

---
+++ apps/products/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+app_name = 'products'
+
+urlpatterns = [
+    # We will leave this empty for a moment until we write our first view
+    # path('', views.ProductListView.as_view(), name='list'),
+]
