609c116af58ae070e6d8a66e398649c1c4e488d8
saleh <saleherragragui02@gmail.com>
2026-02-13 00:13:59 +0100

    adding users/urls.py

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -5,10 +5,8 @@ from . import views
 app_name = 'users'
 
 urlpatterns = [
-    # Placeholder for a profile or dashboard
+    path('register/', views.register, name='register'),
     path('profile/', views.profile, name='profile'),
-    
-    # Built-in Django Login/Logout (Highly recommended for GitHub projects)
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('logout/', auth_views.LogoutView.as_view(next_page='products:home'), name='logout'),
 ]
