420150228e075654651e3e14ca6792d2afa6e6c7
saleh <saleherragragui02@gmail.com>
2026-02-12 09:23:46 +0100

    update the apps/products

--- apps/products/views.py
+++ apps/products/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import ListView
-# Change this line:
-from apps.products.apps import Product 
+# Change '.apps' to '.models'
+from apps.products.models import Product 
 
 def home_temp(request):
     return render(request, 'home.html')
