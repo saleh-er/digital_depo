3b96adb18f115aa649aa7822c7820a05619d75d6
saleh <saleherragragui02@gmail.com>
2026-02-12 23:51:35 +0100

    fix: switch to absolute import for models to resolve nested ModuleNotFoundError

--- apps/products/views.py
+++ apps/products/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.views.generic import ListView
 # Change '.apps' to '.models'
-from .models import Product
+from apps.products.models import Product
 def home_temp(request):
     return render(request, 'home.html')
